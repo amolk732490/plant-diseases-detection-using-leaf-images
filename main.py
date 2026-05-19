2c343328597447209349142b730a6cc1df99bcd9
amolk732490 <amolk732490@gmail.com>
2026-05-19 13:53:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,4 +50,5 @@ async def get_admin_dashboard_stats():
     }
 
 if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
